42ebea051ff9baa982a056e07a4fd2973b0160d0
Benjamin <benmitchell1979@outlook.com>
2018-04-29 16:52:46 -0400

    update runbooks

--- powerDownWebSites.ps1
+++ powerDownWebSites.ps1
@@ -1,1 +1,34 @@
 ## powerDownWebsite Script ##
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+
+        throw $_.Exception
+    }
+}
+
+# Get VM Names and power down 2 of them
+$VMS = Get-AzureRmVM -ResouceGroup azure-automation-demo |? name -like "win-iis-demo*"
+
+foreach ($vm in $vms) {
+    IF ($vm.name -ne 'win-iis-demo-a') {
+        $vm | Start-AzureRmVm -Confirm:$false -Force
+    }
+}
--- powerUpWebsite.ps1
+++ powerUpWebsite.ps1
@@ -1,1 +1,33 @@
 ## powerOnWebsite ##
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+# Get VM Names and power up 2 of them
+$VMS = Get-AzureRmVM -ResouceGroup azure-automation-demo |? name -like "win-iis-demo*"
+
+foreach ($vm in $vms) {
+    IF ($vm.name -ne 'win-iis-demo-a') {
+        $vm | Stop-AzureRmVm -Confirm:$false -Force
+    }
+}
